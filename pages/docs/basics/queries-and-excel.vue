2de60fcafadf3f76ec58594f499b876f0c827ed7
Dimitrie Stefanescu <didimitrie@gmail.com>
2019-06-24 15:43:47 +0100

    added viewer docs

---
+++ pages/docs/basics/queries-and-excel.vue
@@ -0,0 +1,22 @@
+---
+title: "Queries & Excel / PowerBI"
+summary: "How to query a stream to filter for the information you need"
+date: 2019-12-29
+order: 40
+---
+<template lang='md'>
+
+  //TODO
+
+  ## How to query a stream
+
+  ## Examples
+
+  ## Getting your data query into Excel or PowerBI
+
+</template>
+<script>
+export default {
+  layout: 'docs'
+}
+</script>
