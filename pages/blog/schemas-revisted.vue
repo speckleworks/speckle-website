93a8d7a2dc5f107deb790ce1721d27b57bdf1d55
Dimitrie Stefanescu <didimitrie@gmail.com>
2019-07-01 18:32:38 +0100

    finished blog post, starting docs for kits

--- pages/blog/schemas-revisted.vue
+++ pages/blog/schemas-revisted.vue
@@ -1,7 +1,7 @@
 ---
 title: "Bring Your Own Object Models"
 author: Dimitrie
-tags: [ 'announcement' ]
+tags: [ 'object models', 'announcement' ]
 featuredimg: woot.jpg
 summary: "Update on how speckle deals with schemas and standards."
 date: 2019-01-21
@@ -9,6 +9,22 @@ date: 2019-01-21
 
 <template lang='md'>
   Since the end of 2017, a few things have improved and changed in the way Speckle deals with object models and their implementation details in various CAD applications.
+
+  Originally, Speckle embedded its own extensbile object model and allowed for end-user defined data structures (classes) to pass through natively if the receiving party had access to original assembly. You can read more about this in this [early blogpost](/blog/schemas).
+
+  This approach worked well so far, but due to the needs coming out from the community, many of which had existing object models, or were developing others internally, we have changed the architecture of how speckle deals with this aspect of interoperability to allow for more flexibility. **Essentially, we've adopted a management solution to the wicked problem of aec data interoperability.**
+
+  The big news is that you can fully replace speckle's "default" object model with your own. We only enforce minimal conventions to make sure speckle can still efficiently round trip objects and work its other magic. We call these speckle kits, and you can read more about them below:
+
+
+  <div class='text-xs-center my-4'>
+  <v-btn large color='primary' depressed round to='/docs/developers/objectmodels'>
+    Speckle Kit Documentation <v-icon right>arrow_right_alt</v-icon>
+  </v-btn>
+  </div>
+
+  Kits are essentially a set of schema defintions coupled with a series of "implementations" in the software applications where they're relevant. They are dynamically loaded at runtime by speckle core. You can distribute them separately from the Speckle plugins themselves, and you have full rights to license and develop them as you see fit.
+
 </template>
 <script>
 export default {
--- pages/docs/developers/objectmodels.vue
+++ pages/docs/developers/objectmodels.vue
@@ -7,7 +7,69 @@ order: 2
 
 <template lang='md'>
 
-  Speckle has two ways of how to deal with object models, schemas and standards. It is important to know that, by itself, Speckle Core comes with only three default object types, namely `SpeckleObject`, `SpecklePlaceholder` and `SpeckleAbstract`. Everytihng else comes from
+  Speckle has evolved a "management" solution to the wicked problem of interoperability. These are speckle kits, self-contained object models and their implementations in AEC authoring software, that can grow organically, reference each other, and solve parts of the problem.
+
+  It is important to know that, by itself, Speckle Core comes with only three default object types, namely `SpeckleObject`, `SpecklePlaceholder`, and `SpeckleAbstract` and no conversion routines (to/from authoring software). All other schemas (or object defintions) and their conversion routines are coming from dynamically loaded SpeckleKits, which should be independent assemblies that can be freely distributed and licensed.
+
+<!--   <v-alert type='info' :value='true' class='my-4' color='primary'>
+    When installing the Speckle clients, you will automatically get the "default" speckle kits for geometry, structural elements, and some archiectural objects. Speckle would otherwise not be useful out of the box :)
+  </v-alert> -->
+
+  Essentially, a speckle kit is _(1) a set of class defintions_ and _(2) separate projects with the implementation details (conversion to/from native) for authoring software_. {.headline .font-weight-thin .pa-5 .my-5 .text-xs-justify .elevation-12 }
+
+  ## So how does this work?
+
+  Let's envision a simple example. Imagine that you pass an object to speckle for conversion and serialisation:
+
+  ```cs
+
+  // Hint: you can follow along by writing this code and defining
+  // this simple class in a grasshopper c# component!
+
+  var myObject = new TestObject() { foo = 10, bar = "hello" } ;
+  var result = SpeckleCore.Converter.Serialise( myObject );
+  ```
+  <br>
+
+  What will `result` be? The answer hangs on whether there is a SpeckleKit around your `TestObject` class that provides a conversion routine for the current `AppDomain`.
+
+  Let's slow down and unpack the last statement a bit. SpeckleCore, when it's loading inside an a speckle plugin for an authoring application - let's use Grasshopper for now - will look on the users's system for any available speckle kits.
+
+  Kits can reside in two places. SpeckleCore, depending on wether it's built for `Debug` or `Release`, will look for them in:
+  - `%localappdata%/SpeckleKits` for `Release`
+  - `%localappdata%/SpeckleKitsDebug` for `Debug`
+
+  <br>
+
+  If no kits are found, or no `ToSpeckle()` extension methods are present on our `TestObject` class, the conversion routine from SpeckleCore will return a `SpeckleAbstract` object. This is described a bit in [this blogpost](/blog/schemas). In the case of our `TestObject`, it would probably look a bit like this when serialised:
+
+  ```js
+
+  // Hint: if you're following along with grasshopper, set the c# component
+  // output to be a new TestObject, and plug that in a speckle "Serialiser" component.
+
+  {
+    "type": "Abstract",
+    "_type": "TestObject",
+    "_assembly": "myAssembly ...",
+    "hash": "2f71d2004d579e472cf02f714099219b",
+    "geometryHash": "2f71d2004d579e472cf02f714099219b",
+    "properties": {
+      "foo": 10,
+      "bar": "hello"
+    }
+  }
+  ```
+
+
+
+  ## Anatomy of a kit
+
+  Test
+
+  ## Debugging and Deploying a Kit
+
+  Test
 
 </template>
 <script>
