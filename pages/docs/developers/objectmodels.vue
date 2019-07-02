748e35ee71c686041e5eb5e515478088d4187774
Dimitrie Stefanescu <didimitrie@gmail.com>
2019-07-02 15:04:49 +0100

    moar examples

--- pages/docs/developers/objectmodels.vue
+++ pages/docs/developers/objectmodels.vue
@@ -17,7 +17,7 @@ order: 2
 
   Essentially, a speckle kit is _(1) a set of class defintions_ and _(2) separate projects with the implementation details (conversion to/from native) for authoring software_. {.headline .font-weight-thin .pa-5 .my-5 .text-xs-justify .elevation-12 }
 
-  ## So how does this work?
+  ## Quick Example
 
   Let's envision a simple example. Imagine that you pass an object to speckle for conversion and serialisation:
 
@@ -31,13 +31,15 @@ order: 2
   ```
   <br>
 
+  This `TestObject` can either be your own
+
   What will `result` be? The answer hangs on whether there is a SpeckleKit around your `TestObject` class that provides a conversion routine for the current `AppDomain`.
 
   Let's slow down and unpack the last statement a bit. SpeckleCore, when it's loading inside an a speckle plugin for an authoring application - let's use Grasshopper for now - will look on the users's system for any available speckle kits.
 
-  Kits can reside in two places. SpeckleCore, depending on wether it's built for `Debug` or `Release`, will look for them in:
-  - `%localappdata%/SpeckleKits` for `Release`
-  - `%localappdata%/SpeckleKitsDebug` for `Debug`
+  Kits can reside in two places. SpeckleCore, depending on wether it's built for **Debug** or **Release**, will look for them in:
+  - `%localappdata%/SpeckleKits` for **Release**
+  - `%localappdata%/SpeckleKitsDebug` for **Debug**
 
   <br>
 
@@ -61,7 +63,15 @@ order: 2
   }
   ```
 
+  <br>
 
+  Nevertheless, if our `TestObject` class is part of a speckle kit - i.e., MyTestKit, and TestObject does have ToSpeckle() extension method, the converter will choose invoke it and return its result. For example, suppose our conversion logic from TestObject to Speckle is stored in the following method:
+
+  ```cs
+
+   public static SpeckleCircle ToSpeckle( this Circle circ ) {}
+
+  ```
 
   ## Anatomy of a kit
 
