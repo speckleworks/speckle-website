b5fc0b29d23678c11303880cf1f959bffb6d32b9
Mishael Nuh <mishael.nuh@arup.com>
2019-07-16 17:08:51 +0100

    Add schema builder page and finish GSA page.

--- pages/docs/basics/gsa.vue
+++ pages/docs/basics/gsa.vue
@@ -95,7 +95,23 @@ order: 40
   ![revittogsa](~/assets/docs/gsa/revittogsa_fast.gif) {.my-4 .elevation-10}
 
   ### Constructing structural models in Grasshopper
-  //TODO
+  <v-alert type='info' icon='school' :value='true' class='my-4' color='grey'>
+    This tutorial assumes you are comfortable with creating, modifying, and converting Speckle objects in Grasshopper. To learn more, please read the <a href="/docs/advanced/schema-builder"> the following tutorial</a>.
+  </v-alert>
+
+  It is possible to construct a full structural model in Grasshopper using the Speckle plugin. The level of fidelity of the model can be up to the user; you can send simple meshes and lines all the way to full models with section properties, loads, and analysis tasks. For this guide, we will be assuming that the model will be sent to the `design` layer of GSA. In addition, we will be using the SpeckleElement object model. To start, create the geometry of the model using `lines` for 1D members and `meshes` for 2D members. Sending this to GSA will transfer the members.
+
+  ![ghToGsaBasic](~/assets/docs/gsa/ghToGsaBasic.png)
+
+  Using the Grasshopper components, we will assign section properties to our members. This can be done by explicitly converting our `line` and `mesh` to `Structural1DElement` and `Structural2DElementMesh` respectively and attaching some properties to it using the modify component. `Structural2DProperty` and `Structural1DProperty` objects will also be created using the schema builder.
+
+  Properties which includes *Ref* at the end of its name expects the application ID of the Speckle object it is referring to. For example, our `Structural2DElementMesh` expects the application ID of a `Structural2DProperty` for its `PropertyRef` input. If the `ApplicationID` input is not exposed for the `Structural2DProperty`, use the `overwrite custom properties` menu in the context menu to expose it.
+
+  ![ghToGsaProperties](~/assets/docs/gsa/ghToGsaProperties.png)
+
+  Using this pattern, it is possible to create a full fidelity model from Grasshoper for use in GSA (and other structural programs). A demo Grasshopper file can be downloaded [here](/docs/gsa/ghdemo.gh). This model includes everything required to run the analysis: section properties, material properties, loading, analysis tasks, etc. Make sure to set your Rhino units to Millimeters before opening the file and sending the model.
+
+  ![ghToGsaFull](~/assets/docs/gsa/ghToGsaFull.png)
 
   ### Command-line interface
   The GSA plugin has a CLI interface to send and receive GSA models without the use of the interface. For detailed usage help, execute the `SpeckleGSAUI -h` command.
