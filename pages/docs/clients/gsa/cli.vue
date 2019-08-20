2af9e5b5750ad65e047cee8d9aa4070a12a17f07
Mishael Nuh <mishael.nuh@arup.com>
2019-08-20 10:31:45 +0100

    Update GSA CLI docs to use tokens instead of password.

--- pages/docs/clients/gsa/cli.vue
+++ pages/docs/clients/gsa/cli.vue
@@ -5,12 +5,12 @@ date: 2019-08-01
 order: 4
 ---
 <template lang='md'>
-  The GSA plugin has a CLI interface to send and receive GSA models without the use of the interface. For detailed usage help, execute the `SpeckleGSAUI -h` command.
+  The GSA plugin has a CLI interface to send and receive GSA models without the use of the interface. To use it, you will need to obtain the `token` associated with your account, which can be found from the *Profile* page of the Speckle admin app. For detailed usage help, execute the `SpeckleGSAUI -h` command.
 
   Sample sending command:
   ```
 
-  SpeckleGSAUI sender --server "https://hestia.speckle.works/api" --email mishael.nuh@arup.com --password XXX --file "C:\Speckle\Speckle Demos\GSA Demo.gwb" --layer analysis --result "2D Element Derived Force" --resultCases A1
+  SpeckleGSAUI sender --server "https://hestia.speckle.works/api" --email mishael.nuh@arup.com --token "JWT XXXXXX" --file "C:\Speckle\Speckle Demos\GSA Demo.gwb" --layer analysis --result "2D Element Derived Force" --resultCases A1
   ```
 
   <br>
@@ -18,7 +18,7 @@ order: 4
   Sample receiving command:
   ```
 
-  SpeckleGSAUI receiver --server "https://hestia.speckle.works/api" --email mishael.nuh@arup.com --password XXX --file "C:\Users\Mishael.Nuh\Desktop\Received File.gwb" --layer design --nodeAllowance 0.01 --streamIDs YbtzuXrK0W
+  SpeckleGSAUI receiver --server "https://hestia.speckle.works/api" --email mishael.nuh@arup.com --token "JWT XXXXXX" "C:\Users\Mishael.Nuh\Desktop\Received File.gwb" --layer design --nodeAllowance 0.01 --streamIDs YbtzuXrK0W
   ```
 </template>
 <script>
