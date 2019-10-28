f9bbe43159f6036335f8460457f732ba30592f52
Dimitrie Stefanescu <didimitrie@gmail.com>
2019-10-28 14:24:25 +0000

    adds screenshots & links to doc

--- pages/docs/essentials/introduction/interop.vue
+++ pages/docs/essentials/introduction/interop.vue
@@ -12,11 +12,30 @@ order: 6
   - The blog post about [Speckle Kits](/blog/schemas-revisited)
   - Developer Documenation on [schemas and object models](/docs/developers/object-models)
 
+  Below you can find a summary of what currently works and what doesn't, based on the currently available kits:
+
+  ### Core Geometry
+
+  Core geometry is used for Rhino, Grasshopper, Dynamo, a bit in Revit, and threejs (webviewer).
+
+  ![coregeometry](~/assets/docs/essentials/interop/coregeometry.png) {.text-xs-center .my-4}
+
+  ### Elements
+
+  Speckle Elements are used in Revit, and in Grasshopper via the [Schema Builder](/docs/clients/grasshopper/schema-builder) component.
+
+  ![elements](~/assets/docs/essentials/interop/elements.png) {.text-xs-center .my-4}
+
+  ### Structural
+
+  Speckle Elements are used in GSA, and in Grasshopper via the [Schema Builder](/docs/clients/grasshopper/schema-builder) component.
+
+  // TODO
 
   <br>
 
   <div class='text-xs-center my-4'>
-  <v-btn color='primary' round large to='/docs/web/management'>
+  <v-btn color='primary' round large href='https://docs.google.com/spreadsheets/d/1WxZykWf32aEg6z0kN7yzpjJ6cxgg16xIZHAjC4o0eOw/edit?usp=sharing' target="_blank">
     Read more <v-icon right>arrow_right_alt</v-icon>
   </v-btn>
   </div>
