f02a02131116ba31c00d88eb7c29b07f177e63b9
Dimitrie Stefanescu <didimitrie@gmail.com>
2019-10-28 14:10:07 +0000

    adding interop section

---
+++ pages/docs/essentials/introduction/interop.vue
@@ -0,0 +1,30 @@
+---
+title: "Interoperability Matrix"
+summary: "What data transfer is supported between the Speckle plugins"
+date: 2019-10-01
+order: 6
+---
+<template lang='md'>
+
+  Speckle operates on the basis of "lossy interoperability". What this means is that, for example, things might change in transit to better accomodate the host environment in which you are receiving data.
+
+  There's a couple of more in-depth reads on how things actually work behind the scenes, namely:
+  - The blog post about [Speckle Kits](/blog/schemas-revisited)
+  - Developer Documenation on [schemas and object models](/docs/developers/object-models)
+
+
+  <br>
+
+  <div class='text-xs-center my-4'>
+  <v-btn color='primary' round large to='/docs/web/management'>
+    Read more <v-icon right>arrow_right_alt</v-icon>
+  </v-btn>
+  </div>
+
+</template>
+<script>
+export default {
+  layout: 'docs',
+}
+
+</script>
