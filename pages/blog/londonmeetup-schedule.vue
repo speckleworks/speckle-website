09f65c68f11b85de39feb4ba54e8ba0f3b8376c3
Dimitrie Stefanescu <didimitrie@gmail.com>
2019-11-25 08:47:14 +0000

    adds schedule

---
+++ pages/blog/londonmeetup-schedule.vue
@@ -0,0 +1,77 @@
+---
+title: "Speckle Community Meetup Speakers & Schedule"
+author: Dimitrie
+tags: [ 'announcement', 'community' ]
+featuredimg: "~/assets/blog/speakers.png"
+summary: "Speckle Community Meetup Schedule (London, 27th November 2019)"
+date: 2019-11-21
+---
+
+<template lang='md'>
+
+  Some nine amazing hackers stepped up to give a talk at the meetup - big thanks! The event is now sold out, but you can still join the waiting list. Here's the schedule below:
+
+  ![auth](~/assets/blog/speakers.png)
+
+## Intro {.font-weight-light .my-5 }
+
+**5.30 - 6.00** Arrivals and Networking 🍸
+
+**6.00 - 6.15** Opening Talk (Mike Beaven & Rob Greig, Arup)
+
+**6.15 - 6.30** Introduction - Latest Features Sprint (Dimitrie & Matteo)
+
+## Talks {.font-weight-light .my-5 }
+
+**6.30 - 6.45** Arup Carbon: an Embodied Carbon Estimation Tool (Tom Bunn, Arup)
+
+**6.45 - 7.00** Decompose to Compose: Using Speckle to Manage Foreign Structured Models (Guus Gooskens, Arup NL)
+
+**7.00 - 7.15** Seamless Workflows (Fernando Ruiz, Arup UK)
+
+**7.15 - 7.30** Under The Cloud: Design, Build & Operation (Tim Hoffeler, CAD-Development)
+
+## Break 🍸 {.font-weight-light .my-5 }
+
+**7.45 - 8.00** Speckle Admin & Grasshopper Projects (Matthew Swaidan, Woods Bagot)
+
+**8.00 - 8.15** Workflow: Semi-Automated Ventilation Calculations, Room Loads, & Design Report (Izzy Lyseggen, Edinburgh)
+
+**8.15 - 8.30** Speckle Speedround: From GSA to Excel and Beyond (Mishael Nuh, UToronto)
+
+**8.30 - 8.45** Speckle Viz: a UI Prototype for Visualising and Interacting with Speckle's Data Flow (Paul Poinet, UCL)
+
+**8.45 - 9.00** Implementing Speckle: Digital Interoperability as a design & engineering facilitator (Pieter Schreurs & Merijn de Leur, RHDV)
+
+## Closing {.font-weight-light .my-5 }
+
+**9.00 - 9.45** Closing Talk, Drinks, Food & Networking 🍸
+
+**9.45 - 11.00** Pub of Choice 🍻
+
+
+  What is this about? We're organising a community meetup in London, on the 27th of November 2019. Join if you want to hear more about what we've been up to, shape up the roadmap, interact with the community, or simply find out what Speckle actually is - this is your chance: 6pm-9pm, 27th of November @Arup London.
+
+<div class='text-xs-center my-4'>
+  <v-btn large color='primary' outline round href='https://www.eventbrite.co.uk/e/speckle-community-meetup-tickets-77280460939' target="_blank">
+    Join The Waiting List <v-icon right>arrow_right_alt</v-icon>
+  </v-btn>
+</div>
+
+</template>
+<script>
+export default {
+  layout: 'blog',
+  data() {
+    return {
+    }
+  },
+  head() {
+    return {
+      meta: [
+        { hid: 'og-image', property: 'og-image', content: '~/assets/blog/speakers.png' }
+      ]
+    }
+  }
+}
+</script>
