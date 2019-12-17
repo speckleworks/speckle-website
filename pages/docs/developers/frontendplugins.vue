169e9e6476dd1e1eae1d06c453a3d3107e353c44
Dimitrie Stefanescu <didimitrie@gmail.com>
2019-12-17 17:00:05 +0000

    added placeholder  page for frontend plugin docs (ping @mswaidan - i'll do this!)

---
+++ pages/docs/developers/frontendplugins.vue
@@ -0,0 +1,16 @@
+---
+title: "Frontend Plugins"
+summary: "Custom Frontend Plugins For Speckle!"
+date: 2019-12-18
+order: 10
+---
+<template lang='md'>
+
+  // TODO
+
+</template>
+<script>
+export default {
+  layout: 'docs'
+}
+</script>
