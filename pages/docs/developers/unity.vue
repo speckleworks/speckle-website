9bd2b8b200d073013b75a0ff9e51067224ee515e
Jakir Hussain <jakir.hussain@hotmail.co.uk>
2020-03-19 10:42:46 +0000

    Added a page for the unity package under docs/developers rather than clients because it doesn't yet have any sending API and is only really useful as a package for developers that want to do more with a stream AFTER it's been received.

---
+++ pages/docs/developers/unity.vue
@@ -0,0 +1,18 @@
+---
+title: "Unity"
+summary: "The SpeckleUnity Package"
+date: 2020-03-19
+order: 2
+---
+<template lang='md'>
+
+  [![](~/assets/docs/developers/speckleUnity.png)](https://speckleworks.github.io/SpeckleUnity/)
+
+  Thanks to [Jakir Hussain](https://github.com/PabloTheDolphin), the SpeckleUnity documentation now (1) exists and (2) can [be found here!](https://speckleworks.github.io/SpeckleUnity/)
+
+</template>
+<script>
+export default {
+  layout: 'docs'
+}
+</script>
