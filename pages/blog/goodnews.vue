d3c0ea4859a47354ccb6323d3f15762bf511f2f0
Dimitrie Stefanescu <didimitrie@gmail.com>
2020-04-06 17:21:00 +0100

    added announcement

---
+++ pages/blog/goodnews.vue
@@ -0,0 +1,64 @@
+---
+title: "Speckle Futures"
+author: Dimitrie & Matteo
+tags: [ 'announcement', 'community' ]
+summary: "Community announcement"
+date: 2020-04-06
+---
+
+<template lang='md'>
+
+_A joint announcement from Dimitrie, Matteo and Arup_
+
+Good news everyone! Dimitrie and Matteo are founding a company that will focus entirely on Speckle’s development.
+
+<v-card class='px-4 pt-4 pb-2 my-5 elevation-10'>
+
+<p class='title font-weight-thin' style="line-height: 30px !important;">
+Against all odds, we feel the time couldn’t be better for taking this step! Speckle has grown a lot since its birth, it matured as a platform for automation and interoperability with a vibrant and diverse community of users from all corners of the industry. We’re very excited about bringing this journey to the next level and helping everyone take part in the digital transformation of the built environment. Our mission is to work with the amazing Speckle community to grow Speckle as the default OSS data platform of the AEC industry. We’re extremely grateful to Arup for supporting Speckle for the past two years and look forward to continuing to work closely with them.
+</p>
+<p style="text-align: right">
+-- Dimitrie & Matteo
+</p>
+</v-card>
+
+Arup will continue to contribute to the development of both Speckle Server & connectors. David de Koning is Arup’s Speckle Lead and will be coordinating Arup’s contributions to the Speckle community.
+
+<v-card class='px-4 pt-4 pb-2 my-5 elevation-10'>
+
+<p class='title font-weight-thin' style="line-height: 30px !important;">
+It has been a pleasure working closely with Dimitrie and Matteo over the past two years. Speckle is a game-changing platform, and Arup remains committed to its development and adoption, both internally and throughout our industry. I am excited to see and shape the growth of Speckle: both as a great product and a welcoming community!
+</p>
+<p style="text-align: right">
+-- David
+</p>
+</v-card>
+
+Speckle wouldn’t be here without its contributors, past and present, and their amazing contributions. A big thanks goes out to the diverse community, and its generous feedback and enthusiasm, and, last but not least, InnoChain and UCL The Bartlett, for providing the environment in which Speckle was born in late 2015.
+
+
+Speckle is and will always stay Open Source. The only way to change the AEC industry is together, working across organizational, professional and cultural boundaries. {.title .my-4}
+
+Dimitrie, Matteo and Arup are looking forward to working together with our partners in the industry to set up an independent open source foundation to house the project and set it up for continued success — stay tuned for details!
+
+Please reach out to [Dimitrie or Matteo](mailto:hello@speckle.systems) with any questions regarding the Speckle roadmap. Please contact [David](mailto:david.dekoning@arup.com) with any questions regarding the independent Open Source Foundation or Arup’s involvement in the Speckle community.
+
+![arup logo](~assets/specklearup.png) {.my-5}
+
+</template>
+<script>
+export default {
+  layout: 'blog',
+  data() {
+    return {
+    }
+  },
+  head() {
+    return {
+      meta: [
+        { hid: 'og-image', property: 'og-image', content: '~/assets/blog/speakers.png' }
+      ]
+    }
+  }
+}
+</script>
