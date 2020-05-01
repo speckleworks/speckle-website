1e6b60052fb78d562cf41984b9483a28bf141cfc
Dimitrie Stefanescu <didimitrie@gmail.com>
2020-05-01 12:28:49 +0100

    feat(blog): added gsa announcement

---
+++ pages/blog/oasysgsa.vue
@@ -0,0 +1,41 @@
+---
+title: "Speckle GSA Now Open Source 🎊"
+author: Arup, Oasys & Speckle
+tags: [ 'announcement', 'connectors' ]
+summary: "Community announcement"
+date: 2020-05-01
+---
+
+<template lang='md'>
+
+![gsa + speckle](https://www.oasys-software.com/wp-content/uploads/2020/04/GSASpeckle.png)
+
+We are delighted to announce that [Oasys](https://www.oasys-software.com/) has just released SpeckleGSA, an open-source [GSA](https://www.oasys-software.com/products/structural/gsa/) plugin that integrates GSA directly into the Speckle ecosystem. SpeckleGSA opens up new avenues for direct collaboration with architects and designers working in Grasshopper, Rhino, Revit, Dynamo, Excel and the many other design tools that integrate with the Speckle ecosystem.
+
+# Download & Install the latest release [here](https://github.com/arup-group/specklestructuralsuite-installer/releases). {.my-4}
+
+# SpeckleGSA documentation is available [here](https://speckle.systems/docs/clients/gsa/basics). {.my-4}
+
+# The SpeckleGSA source code is available [here](https://github.com/arup-group/speckleGSA). {.my-4}
+
+Over the past year, SpeckleGSA has been used by design teams to deliver automated workflows for metro stations, high rise towers, museums and bridges all around the world. Oasys is pleased to be able to make these capabilities available to all of our GSA customers.
+
+Read the full announcement on [Oasys's website](https://www.oasys-software.com/news/specklegsa-opening-up-new-avenues-for-direct-collaboration/)!
+
+</template>
+<script>
+export default {
+  layout: 'blog',
+  data() {
+    return {
+    }
+  },
+  head() {
+    return {
+      meta: [
+        { hid: 'og-image', property: 'og-image', content: '~/assets/blog/speakers.png' }
+      ]
+    }
+  }
+}
+</script>
