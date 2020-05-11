4fb5ae38fb76ffdcc3b71e734c88f40655b3546b
Jakir Hussain <jakir.hussain@hotmail.co.uk>
2020-05-11 21:27:43 +0100

    Fixed typo in revit docs page. Updated unity docs page and added unreal engine docs page. Docs pages have relevant warnings added.

--- pages/docs/developers/unity.vue
+++ pages/docs/developers/unity.vue
@@ -1,10 +1,14 @@
 ---
 title: "Unity"
 summary: "The SpeckleUnity Package"
-date: 2020-03-19
+date: 2020-05-11
 order: 2
 ---
 <template lang='md'>
+
+  <v-alert type='error' :value='true' class='my-4' color='primary'>
+    The Unity package is WIP. Use it, have fun, but things may break. Or in other words, HIC SVNT DRACONES.
+  </v-alert>
 
   [![](~/assets/docs/developers/speckleUnity.png)](https://speckleworks.github.io/SpeckleUnity/)
 
---
+++ pages/docs/developers/unreal-engine.vue
@@ -0,0 +1,22 @@
+---
+title: "Unreal Engine"
+summary: "The Unreal Engine Plugin"
+date: 2020-05-11
+order: 2
+---
+<template lang='md'>
+
+  <v-alert type='error' :value='true' class='my-4' color='primary'>
+    The Unreal Engine Plugin is WIP. Use it, have fun, but things may break. Or in other words, HIC SVNT DRACONES.
+  </v-alert>
+
+  <v-alert type='info' :value='true' class='my-4' color='primary'>
+    The Unreal Engine plugin is distributed and maintained by <a class='white--text' href='https://www.mobiusnode.io/' target='_blank'>mobiusnode</a>. To install it, <a class='white--text' href='https://github.com/mobiusnode/SpeckleUnreal' target='_blank'>head over the SpeckleUnreal repo</a>.
+  </v-alert>
+  
+</template>
+<script>
+export default {
+  layout: 'docs'
+}
+</script>
