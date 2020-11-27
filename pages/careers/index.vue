3ec1a7dda040621e497208e9e2d1258755f9fdbd
Matteo Cominetti <matteo@cominetti.org>
2020-11-27 17:00:41 +0000

    adds jobs and tweaks careers page a bit

--- pages/careers/index.vue
+++ pages/careers/index.vue
@@ -14,9 +14,8 @@
           <v-card class="elevation-10 white--text pa-5" color="primary">
             <v-card-text>
               <p class="display-2 font-weight-thin">We're hiring!</p>
-              <p class="display-1 font-weight-thin">
-                You could be what we are missing to make Speckle the de-facto
-                data platform for the AEC industry.
+              <p class="display-1 font-weight-thin mt-5">
+                Join our team and help us change the future of the AEC industry.
               </p>
             </v-card-text>
           </v-card>
@@ -29,23 +28,15 @@
               }`
             "
           >
-            <p class="headline font-weight-light mt-4">
-              In early 2020, against all odds, Dimitrie and
-              <a class="flink" href="https://twitter.com/teocomi">Matteo</a>
-              founded a company that would
-              <router-link class="flink" to="/blog/goodnews"
-              >focus exclusively on developing Speckle
-              </router-link
-              >
-              .
+                      <p class="headline font-weight-light mt-2">
+              Tired of corporate environments, endless meetings, middle management and unfulfilling tasks?
             </p>
             <p class="headline font-weight-light mt-5">
-              We are always looking for talented people who believe in our
-              mission and want to change AEC.
+              We are looking for smart and talented individuals who believe in our
+              <a href="http://localhost:3000/blog/speckle2-vision-and-faq">vision</a> and want to change AEC, for the better!
             </p>
             <p class="headline font-weight-light mt-5">
-              If you believe you fit the description, check out our currently
-              open positions below! 👇
+              If you fit the description, check out the positions below and get in touch at <a href="mailto:hello@speckle.systems">hello@speckle.systems</a> 👇
             </p>
           </v-card>
         </v-flex>
@@ -58,41 +49,14 @@
           style="margin-top: -160px"
         ></v-flex>
       </v-layout>
-      <v-layout justify-center row wrap px-5 mt-5 class="text-xs-center">
-        <v-flex xs12 sm8 lg8 xl6 mt-5 py-5 style="min-height:20vh">
-          <h1 class="display-2 mb-3 font-weight-light xxx-text-uppercase">
-            Life @ Speckle
-          </h1>
-          <h2 class="title font-weight-light">Some inspiring paragraph here telling everyone how cool we are!</h2>
-        </v-flex>
-      </v-layout>
-      <v-layout ma-3 justify-center row wrap v-for="(perk, index) in perks"
-                :key="index" :class="index % 2 == 0 ? 'reversed' : ''">
-        <v-flex xs4>
-          <v-layout justify-center>
-            <v-img :src="perk.image" max-height="200" max-width="200" alt="" class="image-round elevation-5"/>
-          </v-layout>
-        </v-flex>
-        <v-flex xs4>
-          <v-layout fill-height row wrap align-center ma-2>
-            <v-flex>
-              <p :class="index % 2 == 0 ? 'text-sm-right' : ''">
-              <span class="title font-weight-bold primary--text">
-                {{ perk.title }}
-              </span>
-                <span class="subheading">{{ perk.description }}</span>
-              </p>
-            </v-flex>
-          </v-layout>
-        </v-flex>
-      </v-layout>
+     
       <v-layout justify-center row wrap px-5 mt-5 class="text-xs-center">
         <v-flex xs12 sm8 lg8 xl6 mt-5 py-5 style="min-height:20vh">
           <h1 class="display-2 font-weight-light xxx-text-uppercase">
             Open positions
           </h1>
           <p class="mt-5 subheading">
-            The world is full of exciting opportunities, these are just some of
+            The world is full of exciting opportunities, and these are some of
             them!
           </p>
         </v-flex>
@@ -196,12 +160,40 @@
                     believe in our mission and want to change AEC.
                     <v-divider class="my-3"/>
                     If you're intrested in working at Speckle, get in touch at
-                    hello@speckle.com.
+                    hello@speckle.systems
                   </div>
                 </v-flex>
               </v-layout>
             </v-card>
           </v-hover>
+        </v-flex>
+      </v-layout>
+       <v-layout justify-center row wrap px-5 mt-5 class="text-xs-center">
+        <v-flex xs12 sm8 lg8 xl6 mt-5 py-5 style="min-height:20vh">
+          <h1 class="display-2 mb-3 font-weight-light xxx-text-uppercase">
+            Life @ Speckle
+          </h1>
+          <h2 class="title font-weight-light">Wondering what it's like to work with us? Here's some of our perks!</h2>
+        </v-flex>
+      </v-layout>
+      <v-layout ma-3 justify-center row wrap v-for="(perk, index) in perks"
+                :key="index" :class="index % 2 == 0 ? 'reversed' : ''">
+        <v-flex xs4>
+          <v-layout justify-center>
+            <v-img :src="perk.image" max-height="200" aspect-ratio="1"  alt="" contain class=""/>
+          </v-layout>
+        </v-flex>
+        <v-flex xs4>
+          <v-layout fill-height row wrap align-center ma-2>
+            <v-flex>
+              <p :class="index % 2 == 0 ? 'text-sm-right' : ''">
+              <span class="title font-weight-bold primary--text">
+                {{ perk.title }}
+              </span>
+                <span class="subheading">{{ perk.description }}</span>
+              </p>
+            </v-flex>
+          </v-layout>
         </v-flex>
       </v-layout>
     </v-container>
@@ -299,58 +291,71 @@ export default {
       strapiBaseUri: process.env.strapiBaseUri,
       perks: [
         {
-          title: "No office!",
-          description: "Work from home, 100% of the time.",
-          image: "https://via.placeholder.com/200"
+          title: "Awesome company retreats",
+          description: "... or at least they will be, once we are allowed to travel again!",
+          image: "https://user-images.githubusercontent.com/2679513/100470777-ccaaab80-30d0-11eb-945f-594a6219994d.png"
+        },
+        {
+          title: "No office, no commute!",
+          description: "Work from home or from wherever you fancy the most, 100% of the time.",
+          image: "https://media4.giphy.com/media/XymXANrzgI4k6FL6zr/giphy.gif"
         },
         {
           title: "Flexible hours",
           description: "There is no Big Brother watching you. If you need to take your bunnies to the vet, just do so.",
-          image: "https://via.placeholder.com/200"
+          image: "https://discourse.speckle.works/uploads/default/optimized/1X/e77a5e5d33d47990227ef47d418e64d386716891_2_666x500.jpeg"
         },
         {
-          title: "Choose your setup",
-          description: "We don't really care if you are a Mac, Windows or 🖖🏼 person. Choose the hardware/software that makes you happy.",
-          image: "https://via.placeholder.com/200"
+          title: "Choose your own gear",
+          description: "You'll get a generous gadget budget to spend on the hardware & software that makes you happy.",
+          image: "https://expanscape.com/assets/images/A7_6.jpg"
         },
         {
           title: "No BS meetings",
-          description: "Let's face it, we all hate useless meetings. Stand-ups don't count! 🤣",
-          image: "https://via.placeholder.com/200"
+          description: "Let's face it, we all hate useless meetings. PS Stand-ups don't count! 🤣",
+          image: "https://i.pinimg.com/236x/ef/35/40/ef354038963cd0dec07bb62aaaa04c94.jpg"
         },
-        {
-          title: "Loose dress code",
-          description: "Want to NOT wear trousers? That's fine by us (as long as you don't stand up during meetings...)",
-          image: "https://via.placeholder.com/200"
-        },
+        // {
+        //   title: "Loose dress code",
+        //   description: "Want to NOT wear trousers? That's fine by us (as long as you don't stand up during meetings...)",
+        //   image: "https://via.placeholder.com/200"
+        // },
         {
           title: "Bonkers Stand-Ups",
-          description: "Lets just say: we don't do meetings like others do...",
-          image: "https://via.placeholder.com/200"
-        },
-        {
-          title: "Awesome company retreats",
-          description: "... or at least they will be, once 2020 is over!",
-          image: "https://via.placeholder.com/200"
+          description: "Our morning ritual is never boring!",
+          image: "https://user-images.githubusercontent.com/2679513/100470143-93257080-30cf-11eb-83ed-a3dde755c543.png"
         }
       ],
       positions: [
         {
-          title: "C# Dev",
-          subtitle: "Whatever whatever...",
+          title: "AEC Software Engineer",
+          subtitle: "C#, WPF, AEC APIs, GraphQL",
           description:
-            "A geeky person with experience in X and Y, knowledge of common AEC desktop software SDKs such as Revit, Rhino... and whatever else etc etc...",
+            "If software APIs are your daily bead 🍞, then this position is for you!"+
+            " We're looking for an experienced developer who has tamed various authoring and analysis tools APIs (Revit, Rhino, GHrasshopper, Dynamo  APIs...)"+
+            " to help us support and expand our growing list of connectors. You will be in charge of our .NET SDK. Web development / Python skills are a plus! ",
           image:
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLhkgNhzZVREUuVKM34OVtosBGlTCR00Ymqg&usqp=CAU",
           url: "/"
         },
         {
-          title: "JS Dev",
-          subtitle: "Whatever for javascript...",
+          title: "Frontent Engineer",
+          subtitle: "Js, Vue, UX/UI, GraphQL",
           description:
-            "A geeky person with experience in X and Y, knowledge of common Node.js, GraphQL... TBD",
+            "We're looking for an experienced and talented Frontent + UX/UI engineer to look after and improve the Speckle Web app and to develop brand new specialized apps that will leverage" +
+            " Speckle data (dashboards, automation & management tools and more) and experiences. AEC / Node.js / Three.js skills are a plus!",
           image:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoPhoZldr8xb_FQDHgmv5QOmH1PFkeumJjuw&usqp=CAU",
+            "https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg",
+          url: "/"
+        },
+                {
+          title: "3D Web Engineer",
+          subtitle: "Three.js, WebGL, 3D",
+          description:
+            "If 3D geometry in the browser is your passion, then this position is for you! We're looking for a skilled Three.js/WebGL developer to bring our viewer to the next level." +
+            " You'll be responsible of the coolest open source viewer for the AEC industry! AEC / Frontend experience are a plus!",
+          image:
+            "https://img.stackshare.io/service/5883/preview.png",
           url: "/"
         }
       ]
