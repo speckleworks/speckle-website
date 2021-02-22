671c1888245cfdcf8e48426c4af1b6f9c10a41a9
Matteo Cominetti <matteo@cominetti.org>
2021-02-22 16:51:56 +0000

    feat: adds v2 banner

--- layouts/blog.vue
+++ layouts/blog.vue
@@ -1,5 +1,25 @@
 <template>
   <v-app :dark="$store.state.dark">
+    <v-snackbar
+      class="mt-5 pa-5"
+      :value="true"
+      color="primary"
+      dark
+      :multi-line="true"
+      top
+      :timeout="0">
+      <div>
+        <h3 class="pa-0 ma-0">🚀 A brand new Speckle has arrived, check it out!</h3>
+        <p class="pt-1 ma-0">Psss! You are browsing an old version, time to upgrgade 😉</p>
+      </div>
+      <v-btn
+        text
+        color="blue"
+        href="https://speckle.systems"
+      >
+        Speckle v2
+      </v-btn>
+    </v-snackbar>
     <Toolbar></Toolbar>
     <v-content>
       <v-container grid-list-md>
